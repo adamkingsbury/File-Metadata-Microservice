631ab1d51ac87ef58cdcd43a8d26105b58467e4e
Adam Kingsbury <adam.kingsbury@gmail.com>
2018-03-15 08:56:40 +0000

    init image detail view component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ImageHistoryComponent } from './image-history/image-history.component';
 import { ImageHistoryService } from './image-history/image-history.service';
+import { ImageDetailComponent } from './images/image-detail/image-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageHistoryComponent
+    ImageHistoryComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule,

2275dba0d10279276773c453e5b5370be4aa05be
Adam Kingsbury <adam.kingsbury@gmail.com>
2018-03-17 04:15:14 +0000

    generated new component closes #4

--- client/src/app/images/images.module.ts
+++ client/src/app/images/images.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ImageDetailComponent, ImageDetailContent } from './image-detail/image-detail.component';
 import { ImageHistoryComponent } from './image-history/image-history.component';
 import { ImageHistoryService } from './service/image-history.service';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,7 @@ import { ImageHistoryService } from './service/image-history.service';
   ],
   declarations: [
     ImageDetailComponent, ImageDetailContent, //These two are found in the same file
-    ImageHistoryComponent
+    ImageHistoryComponent, ImageUploadComponent
   ],
   entryComponents: [ImageDetailContent],
   providers: [ImageHistoryService, ImageDetailComponent],

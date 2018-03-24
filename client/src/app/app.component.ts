f568e1ddd4664678989f45d8f72869d1ea6e0cb4
Adam Kingsbury <adam.kingsbury@gmail.com>
2018-03-24 13:16:36 +0000

    Change to heading and title of app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'File Metatdata Analysis Microservice';
+  title = 'Image Metadata Analysis Microservice';
 }
